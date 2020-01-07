66923fca57c90c516c602c4569899f88a15e6458
Anders Weiskvist <Weiskvist94@gmail.com>
2020-01-07 11:01:44 +0100

    Add project files.

---
+++ Authorization/Data/AuthorizationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.Models
+{
+    public class AuthorizationContext : DbContext
+    {
+        public AuthorizationContext (DbContextOptions<AuthorizationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Authorization.Models.User> User { get; set; }
+    }
+}
---
+++ Authorization/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization.Models
+{
+    public class User
+    {
+        public int ID{ get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
